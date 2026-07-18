458bfa90a891482b9961e84cd2760922c8e72ed4
Adarsh Dixit <adarshdix002@gmail.com>
2026-07-18 17:14:48 +0530

    Implement Category module with full CRUD (Controller-Service-Repository pattern)

---
+++ src/test/java/com/retailinventory/retailinventorysystem/RetailInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailinventory.retailinventorysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
